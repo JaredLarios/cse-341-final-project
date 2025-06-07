66aa74e3a194f31c285181a0428ee3428c21093f
JaredLarios <lar21038@byui.edu>
2025-06-07 12:00:50 -0600

    feat: enhance CORS configuration to include specific headers and methods

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app
     .use(bodyParser.json())
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         next();
     })
     .use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }))
